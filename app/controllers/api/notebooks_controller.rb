3765528951347e509c795719e717767bc67797e2
Satish Kunisi <kunisi@gmail.com>
2014-01-15 11:48:16 -0800

    Add NotebooksController#show #create #update #destroy

--- app/controllers/api/notebooks_controller.rb
+++ app/controllers/api/notebooks_controller.rb
@@ -3,18 +3,34 @@ class Api::NotebooksController < ApplicationController
 
   def index
     @notebooks = current_user.notebooks
-    render :index
   end
 
   def create
+    @notebook = Notebook.new(params[:notebook])
   end
 
   def destroy
+    @notebook = Notebook.find(params[:id])
+    @notebook.destroy!
   end
 
   def update
+    @notebook = Notebook.find(params[:id])
+
+    if @notebook.update_attributes(params[:notebook])
+      render :update
+    else
+      render :status => 500
+    end
   end
 
   def show
+    @notebook = Notebook.find(params[:id])
+
+    if @notebook
+      render :show
+    else
+      render :status => 500
+    end
   end
 end
